e6e358b94545d7789eb49c94a028ff05d242b6ba
Carlosdb148 <92909837+Carlosdb148@users.noreply.github.com>
2022-12-19 20:49:28 +0100

    Delete InterfazEscritorio directory

--- InterfazEscritorio/index.js
+++
@@ -1,7 +0,0 @@
-$( "button" ).click(function() {
-    $( ".desktop" ).append( "<div class='ui-widget-content'>"+ $( ".text" ).val() +"</div>" );
-    $( function() {
-        $( ".ui-widget-content" ).draggable();
-      });
-});
-
